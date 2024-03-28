c2c7187185ead3edbe6be1ffc2a8bd1009da02c3
Ahmad Habibi <ahmadhabibi7159@gmail.com>
2024-03-28 17:15:30 +0800

    iewrhjiushdruithieofh

--- server/server.go
+++ server/server.go
@@ -5,13 +5,12 @@ import (
 	"fmt"
 	"sync/atomic"
 
+	"github.com/ahmadhabibi14/gnet-starter/protocol"
 	"github.com/panjf2000/gnet/v2"
 	"github.com/panjf2000/gnet/v2/pkg/logging"
-
-	"github.com/ahmadhabibi14/gnet-starter/protocol"
 )
 
-type simpleServer struct {
+type Server struct {
 	gnet.BuiltinEventEngine
 	eng          gnet.Engine
 	network      string
@@ -21,53 +20,55 @@ type simpleServer struct {
 	disconnected int32
 }
 
-func (s *simpleServer) OnBoot(eng gnet.Engine) (action gnet.Action) {
-	logging.Infof("running server on %s with multi-core=%t",
-		fmt.Sprintf("%s://%s", s.network, s.addr), s.multicore)
+func NewServer(network string, port int, multicore bool) *Server {
+	return &Server{
+		network: network,
+		addr: fmt.Sprintf(":%d", port),
+		multicore: multicore,
+	}
+}
+
+func (s *Server) OnBoot(eng gnet.Engine) (action gnet.Action) {
+	logging.Infof(
+		"running server on %s with multicore=%t",
+		fmt.Sprintf("%s://%s", s.network, s.addr), s.multicore,
+	)
 	s.eng = eng
 	return
 }
 
-func (s *simpleServer) OnOpen(c gnet.Conn) (out []byte, action gnet.Action) {
-	c.SetContext(new(protocol.SimpleCodec))
-	atomic.AddInt32(&s.connected, 1)
-	out = []byte("sweetness\r\n")
-	return
+func (s *Server) OnShutdown(eng gnet.Engine) {
+	logging.Infof("server close")
 }
 
-func (s *simpleServer) OnClose(c gnet.Conn, err error) (action gnet.Action) {
+func (s *Server) OnClose(c gnet.Conn, err error) (action gnet.Action) {
 	if err != nil {
 		logging.Infof("error occurred on connection=%s, %v\n", c.RemoteAddr().String(), err)
 	}
 	disconnected := atomic.AddInt32(&s.disconnected, 1)
 	connected := atomic.AddInt32(&s.connected, -1)
 	if connected == 0 {
-		logging.Infof("all %d connections are closed, shut it down", disconnected)
-		action = gnet.Shutdown
+		logging.Infof("all %d connections are closed", disconnected)
+		action = gnet.None
 	}
 	return
 }
 
-func (s *simpleServer) OnTraffic(c gnet.Conn) (action gnet.Action) {
-	codec := c.Context().(*protocol.SimpleCodec)
-	var packets [][]byte
-	for {
-		data, err := codec.Decode(c)
-		if err == protocol.ErrIncompletePacket {
-			break
-		}
-		if err != nil {
-			logging.Errorf("invalid packet: %v", err)
-			return gnet.Close
-		}
-		packet, _ := codec.Encode(data)
-		packets = append(packets, packet)
-	}
-	if n := len(packets); n > 1 {
-		_, _ = c.Writev(packets)
-	} else if n == 1 {
-		_, _ = c.Write(packets[0])
-	}
+
+func (s *Server) OnOpen(c gnet.Conn) (out []byte, action gnet.Action) {
+	c.SetContext(new(protocol.SimpleCodec))
+	atomic.AddInt32(&s.connected, 1)
+	out = []byte("sweetness\r\n")
+	return
+}
+
+func (s *Server) OnTraffic(c gnet.Conn) (action gnet.Action) {
+	buf := make([]byte, 1024)
+	_, err := c.Read(buf)
+	logging.Error(err)
+	fmt.Printf("Received: %s\n", buf)
+
+	action = gnet.None
 	return
 }
 
@@ -79,11 +80,10 @@ func main() {
 	flag.IntVar(&port, "port", 9000, "--port 9000")
 	flag.BoolVar(&multicore, "multicore", false, "--multicore=true")
 	flag.Parse()
-	ss := &simpleServer{
-		network:   "tcp",
-		addr:      fmt.Sprintf(":%d", port),
-		multicore: multicore,
-	}
-	err := gnet.Run(ss, ss.network+"://"+ss.addr, gnet.WithMulticore(multicore))
+	
+	server := NewServer("tcp", port, multicore)
+	protoAddr := server.network+"://"+server.addr
+
+	err := gnet.Run(server, protoAddr, gnet.WithMulticore(multicore))
 	logging.Infof("server exits with error: %v", err)
 }
